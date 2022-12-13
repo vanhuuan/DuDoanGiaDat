9536b30ea60e7c2752a232746612e0422051900f
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-12-13 21:01:52 +0700

    update a little bit

--- DuDoan/views.py
+++ DuDoan/views.py
@@ -7,7 +7,10 @@ from tensorflow.python.keras.models import load_model
 from sklearn.preprocessing import RobustScaler
 import numpy as np
 import math
-from DuDoanGiaDat import settings
+from sklearn import preprocessing
+import pandas as pd
+
+le = preprocessing.LabelEncoder()
 
 
 # Create your views here.
@@ -26,6 +29,58 @@ def ketqua(request):
     bedroom = request.POST['bedroom']
     floor = request.POST['floor']
     direct = request.POST['direct']
-    paper = request.POST['paper']
-    print(road, type, width, length, area, bedroom, floor, direct, paper)
+    print(road, type, width, length, area, bedroom, floor, direct)
+    newRc = {
+        'Road': road,
+        'Type Of Estate': type,
+        'Square': area,
+        'Width': width,
+        'Length': length,
+        'Floors': floor,
+        'Bedrooms': bedroom,
+        'Direction': direct
+    }
+    df = pd.read_csv('dataclean.csv')
+    df = df[['Road','Type Of Estate','Square','Width','Length','Floors','Bedrooms','Direction']]
+    df = fixOutlier(df)
+    df = df.append(newRc, ignore_index=True)
+    df['Type Of Estate'] = le.fit_transform(df['Type Of Estate'])
+    df['Direction'] = le.fit_transform(df['Direction'])
+    df = ChuanHoa(df)
+    # predicted = model.predict(df.iloc[-1])
+    # print(predicted)
     return render(request, 'ketqua.html', {'price': 1000000})
+
+
+def fixOutlier(train_df):
+    index = []
+    for i in range(train_df.shape[0]):
+        if i == 0:
+            continue
+        if train_df['Square'].iloc[i] >= 450:
+            if i not in index:
+                index.append(i)
+        elif train_df['Length'].iloc[i] > 60:
+            if i not in index:
+                index.append(i)
+        elif train_df['Width'].iloc[i] > 50:
+            if i not in index:
+                index.append(i)
+        elif train_df['Floors'].iloc[i] > 18:
+            if i not in index:
+                index.append(i)
+        elif train_df['Road'].iloc[i] > 100:
+            if i not in index:
+                index.append(i)
+        elif train_df['Bedrooms'].iloc[i] > 40:
+            if i not in index:
+                index.append(i)
+    train_df = train_df.drop(index)
+    return train_df
+
+
+def ChuanHoa(train_df):
+    scaler = RobustScaler()
+    num_cols = ['Road','Square','Width','Length','Floors','Bedrooms']
+    train_df[num_cols] = scaler.fit_transform(train_df[num_cols])
+    return train_df
